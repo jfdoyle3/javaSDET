0a1a389e33456ed26ff2b43f299cf123cfb6f6b1
Jim Doyle <indellibleink@gmail.com>
2019-12-21 11:38:55 -0500

    Project outputs 1 record to a json file

--- JsonJava/src/jsonToJava.java
+++ JsonJava/src/jsonToJava.java
@@ -1,16 +1,26 @@
 import java.sql.DriverManager;
+import java.io.File;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
+
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.sql.ResultSet;
 
 public class jsonToJava {
 
-	public static void main(String[] args) throws ClassNotFoundException, SQLException {
-		// TODO Auto-generated method stub
-	   
+	public static void main(String[] args) throws ClassNotFoundException, SQLException,IOException {
+  
 	   Class.forName("com.mysql.cj.jdbc.Driver");
 	   Connection conn=null;
+	   
+	   CustomerDetails customer=new CustomerDetails();
+	   
+	   // Connect to Database
 	   conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/business", "root", "DBadmin");
 	   System.out.println("Connected to Database");
 	   
@@ -37,18 +47,23 @@ public class jsonToJava {
 			 * System.out.println(queryResult.getInt(3));
 			 * System.out.println(queryResult.getString(4));
 			 */
-		   // With getters / setters:
-		   CustomerDetails customer=new CustomerDetails();
+		   // With getters| setters: Using Setters:
+		   
 		   customer.setCourseName(queryResult.getString(1));
 		   customer.setPurchasedDate(queryResult.getString(2));
 		   customer.setAmount(queryResult.getInt(3));
 		   customer.setLocation(queryResult.getString(4));
 		   
-		   System.out.println(customer.getCourseName());
-		   System.out.println(customer.getPurchasedDate());
-		   System.out.println(customer.getAmount());
-		   System.out.println(customer.getLocation());
+			/* Console Output with getters:
+			 * System.out.println(customer.getCourseName());
+			 * System.out.println(customer.getPurchasedDate());
+			 * System.out.println(customer.getAmount());
+			 * System.out.println(customer.getLocation());
+			 */
 	  }
+	   ObjectMapper customerObj=new ObjectMapper();
+	   customerObj.writeValue(new File("D:\\repository\\SDET\\javaSDET\\JsonJava\\customerInfo.json"), customer);
+	   System.out.println("File Created");
 	  conn.close();
 	 }
 
