c8114a5c3e4cb8371ddbfbbe56aba87eeddf5f93
Jim Doyle <indellibleink@gmail.com>
2019-12-21 21:19:16 -0500

    Completed: using a DB to make a Json File.

--- JsonJava/src/oneSingleJson.java
+++ JsonJava/src/oneSingleJson.java
@@ -1,18 +1,24 @@
 import java.sql.DriverManager;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 
+import org.apache.commons.text.StringEscapeUtils;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
 import com.fasterxml.jackson.core.JsonGenerationException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
 
 import java.sql.ResultSet;
 
-public class jsonToJava {
+public class oneSingleJson {
 
 	public static void main(String[] args) throws ClassNotFoundException, SQLException,IOException {
   
@@ -20,6 +26,8 @@ public class jsonToJava {
 	   Connection connctn=null;
 	   
 	   ArrayList<CustomerDetails> customerList=new ArrayList<CustomerDetails>();
+	   JSONArray jsonArr=new JSONArray();
+	   
 	   // Connect to Database
 	   connctn=DriverManager.getConnection("jdbc:mysql://localhost:3306/business", "root", "DBadmin");
 	   System.out.println("Connected to Database");
@@ -66,9 +74,36 @@ public class jsonToJava {
 	   {
 	   ObjectMapper customerObj=new ObjectMapper();
 	   customerObj.writeValue(new File("D:\\repository\\SDET\\javaSDET\\JsonJava\\customerInfo"+index+".json"), customerList.get(index));
+	   
+	   // Gson: Create json String from Java Object
+	   Gson gson=new Gson();
+	   String jsonString=gson.toJson(customerList.get(index));
+	   jsonArr.add(jsonString);
 	   }
+	   
 	   System.out.println(customerList.size()+" Files Created");
+	   
+
+	   // Json Simple: .put("data",jsonArr) .  "Data" is json header /array name. jsonArray is the data array
+	   JSONObject jsonObj=new JSONObject();
+	   jsonObj.put("data",jsonArr);
+	   
+	   // Apache Commons Text
+	   String jsonEscRemoved=StringEscapeUtils.unescapeJava(jsonObj.toJSONString());
+	   String stringMod1=jsonEscRemoved.replace("\"{","{");
+	   String cleanJsonResult=stringMod1.replace("}\"", "}");
+	   System.out.println(cleanJsonResult);
+	   
+	   // Write to a file
+	   try (FileWriter jsonFile=new FileWriter("D:\\repository\\SDET\\javaSDET\\JsonJava\\CustomerData.json"))
+	   {
+		   jsonFile.write(cleanJsonResult);
+		   
+	   }
+	   System.out.println("CustomerData.json written");
+	   
 	  connctn.close();
+	   System.out.println("Connection Closed");
 	 }
 
 }
